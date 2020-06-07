7acd2a8c64025c5bcd1dcf26c171a9d00ac1925d
sofia3484 <60732240+sofia3484@users.noreply.github.com>
2020-06-07 23:14:39 +0700

    Add files via upload

---
+++ Database-Lokal/ui.R
@@ -0,0 +1,53 @@
+# Flight-Dashboard using Shiny. 
+# Written by : Sofia Nicklaus S.
+# Department of Business statistics, Matana University (Tangerang)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+source("global.R", local = TRUE)
+
+ui <- dashboardPage(title = "www.sofianicklaus.com",
+                    
+      # Header
+      dashboardHeader(title = "Flights-Dashboard",titleWidth = 200),
+                    
+      # Side bar of the Dashboard
+      dashboardSidebar(
+        selectInput(
+          inputId = "airline",
+          label = "Airline:",
+          choices = airline_list,
+          selected = "DL",
+          selectize = FALSE),
+        
+        
+      # Side menu of the Dashboard  
+      sidebarMenu(
+        selectInput(
+          inputId = "month",
+          label = "Month:",
+          choices = month_list,
+          selected = 99,
+          size = 13,
+          selectize = FALSE),
+        actionLink("remove", icon = icon("sync-alt"),"Remove detail tabs"),
+        menuItem("Source Code", icon = icon("github"), href = "https://github.com/sofia3484/Flights-Dashboard"),
+        menuItem("About Me", icon = icon("linkedin"), href = "https://www.linkedin.com/in/sofia-ns/"),
+        menuItem("Video", icon = icon("youtube"), href = "https://youtu.be/N1ycn8ZVcuM")
+        )
+      ),
+      
+      # The body of the dashboard
+      dashboardBody(
+        tabsetPanel(id = "tabs",
+          tabPanel(title = "Main Dashboard",
+          value = "page1",
+        fluidRow(valueBoxOutput("total_flights"),
+                 valueBoxOutput("per_day"),
+                 valueBoxOutput("percent_delayed")),
+        fluidRow(column(width = 6,d3Output("group_totals")),
+                 column(width = 6,d3Output("top_airports")))
+      )
+    )
+  )
+)
